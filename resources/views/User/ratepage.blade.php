ce9c71abb3e60cc6d8e5f542c4fe5e8f254e7526
AhmedAdel <ahmedadelfahim@gmail.com>
2020-03-16 11:29:07 +0200

    adding borrow functionality to single book page

--- resources/views/User/ratepage.blade.php
+++ resources/views/User/ratepage.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.ratelayout')
 
 @section('content')
-<div class="row">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    <div class="row">
   <div class="flex-card card">
     <div>
        <img src="{{url('uploads/'.$book->pic)}}"/>
@@ -25,7 +27,9 @@
     @else
         <span style="margin-bottom: 20px">{{ $book->quantity }} copies available</span>
     @endif
-        <a id="lease" class="btn btn-success btn-sm" href="#">Lease</a> 
+        <button id="lease" data-toggle="modal" data-title="{{$book->title}}" data-book_id="{{$book->id}}"
+        data-target="#borrow-model" class="btn btn-success btn-sm btn-block lease"
+        {{ $book->quantity <= 0?"disabled":"" }}>Lease</button>
     </div>
 </div>
 
@@ -201,4 +205,45 @@
   @endforeach
 </div>
 <script src="{{ asset('/dist/js/rate.js') }}"></script>
+<script src="{{asset('js/library_home.js')}}"> </script>
 @endsection
+
+{{--borrow model--}}
+<div class="modal fade" id="borrow-model"
+     tabindex="-1" role="dialog"
+     aria-labelledby="borrowModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header d-flex flex-row align-self-start">
+                <button type="button" class="close mr-1"
+                        data-dismiss="modal"
+                        aria-label="Close">
+                    <span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title"
+                    id="borrowModalLabel"></h4>
+            </div>
+            <form method="post" action="{{ route('borrows.store') }}">
+                @csrf
+                <input name="book_id" id="book_id" hidden/>
+                <div class="modal-body">
+                    <p>
+                        Please confirm you would like to borrow
+                        <b><span id="book-title"></span></b>
+                        for <input name="numberOfDays" type="number" class="w-25 d-inline" min="1" value="1"/> days
+                    </p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button"
+                            class="btn btn-default"
+                            data-dismiss="modal">Close</button>
+                    <span class="pull-right">
+          <button type="submit" class="btn btn-success">
+            Borrow
+          </button>
+        </span>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
